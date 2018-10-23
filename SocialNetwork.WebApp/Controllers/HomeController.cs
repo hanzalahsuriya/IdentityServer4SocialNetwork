6f6037ee3ad66149414c30484bc93faa58b97d67
hanzalahsuriya <hanzalah@gmail.com>
2018-10-24 00:22:47 +0100

    refresh token

--- SocialNetwork.WebApp/Controllers/HomeController.cs
+++ SocialNetwork.WebApp/Controllers/HomeController.cs
@@ -6,6 +6,11 @@ using SocialNetwork.Web.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using System.Net.Http.Headers;
+using IdentityModel.Client;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace SocialNetwork.WebApp.Controllers
 {
@@ -19,6 +24,7 @@ namespace SocialNetwork.WebApp.Controllers
         [Authorize]
         public async Task<ActionResult> Shouts()
         {
+            await RefreshToken();
             var accessToken = await HttpContext.GetTokenAsync("access_token");
             using (var client = new HttpClient())
             {
@@ -78,6 +84,45 @@ namespace SocialNetwork.WebApp.Controllers
         {
             await HttpContext.SignOutAsync("Cookies");
             await HttpContext.SignOutAsync("oidc");
+        }
+
+        public async Task RefreshToken()
+        {
+            var authorizationServerInformation = await DiscoveryClient.GetAsync("http://localhost:5000");
+            var client = new TokenClient(authorizationServerInformation.TokenEndpoint, "socialnetwork_hybrid", "client_secret");
+            var refreshToken = await HttpContext.GetTokenAsync("refresh_token");
+            var tokenResponse = await client.RequestRefreshTokenAsync(refreshToken);
+            var identityToken = await HttpContext.GetTokenAsync("id_token");
+
+            var expiresAt = DateTime.UtcNow + TimeSpan.FromSeconds(tokenResponse.ExpiresIn);
+
+            var tokens = new[]
+            {
+                new AuthenticationToken()
+                {
+                    Name = OpenIdConnectParameterNames.IdToken,
+                    Value = identityToken
+                },
+                new AuthenticationToken()
+                {
+                    Name = OpenIdConnectParameterNames.AccessToken,
+                    Value = tokenResponse.AccessToken
+                },
+                new AuthenticationToken()
+                {
+                    Name = OpenIdConnectParameterNames.RefreshToken,
+                    Value = tokenResponse.RefreshToken
+                },
+                new AuthenticationToken()
+                {
+                    Name = "expires_at",
+                    Value = expiresAt.ToString("o", CultureInfo.InvariantCulture)
+                }
+            };
+
+            var authenticationInformation = await HttpContext.AuthenticateAsync("Cookies");
+            authenticationInformation.Properties.StoreTokens(tokens);
+            await HttpContext.SignInAsync("Cookies", authenticationInformation.Principal, authenticationInformation.Properties);
         }
     }
 }
